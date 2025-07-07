ed7fd20be3a49e5b65207ea68da2f243b3e2d208
AngusWebbHammond <anguswebbhammond@gmail.com>
2025-07-07 11:58:07 +0100

    fixed minor issues with shaders

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
 "void main()\n"
 "{\n"
-"gl_Position = vec4(aPos.x,aPos.y,aPos.z,1.0f\n"
+"gl_Position = vec4(aPos.x,aPos.y,aPos.z,1.0f);\n"
 "}\0";
 
 // Fragment shader source code
@@ -21,7 +21,7 @@ const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
-"FragColor = vec4(0.8f,0.3f,0.02f,1.0f)\n"
+"FragColor = vec4(0.8f,0.3f,0.02f,1.0f);\n"
 "}\n\0";
 
 const GLuint WIDTH = 800, HEIGHT = 600;
